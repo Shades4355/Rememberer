b0af9e3a62c921b9e2c2c96905780363ef7a5b8d
Shades <mydude@Shadess-MBP.home>
2020-06-22 01:19:58 -0400

    React for home page started

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,33 @@
 import React from 'react'
 
+import AppList from './AppList'
+
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  let appsArray = null
+  let apps
+
+  if (appsArray === null) {
+    apps = (
+      <div>
+        Apps Coming Soon!
+      </div>
+    )
+  } else {
+    apps = appsArray.map(singleApp => {
+      return <AppList singleApp={singleApp} />
+    })
+  }
+
+  return (
+    <div>
+      <h1>Welcome to Remembery!</h1>
+      <div>
+        <ul>
+          {apps}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ app/javascript/react/components/AppList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AppList = props => {
+
+  return (
+    <div>
+      <li>
+        {props.singleApp}
+      </li>
+    </div>
+  )
+}
+
+export default AppList
